75804fd1affb87467420a50845f62b7d8731d988
ben waldee <b1.waldee@gmail.com>
2022-08-02 15:52:31 -0500

    completed Get details of a spot from an id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -7,38 +7,6 @@ const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-
-router.get('/', async (req, res) => {
-    const Spots = await Spot.findAll({
-
-        attributes: {
-            include: [
-                [
-                    sequelize.fn("AVG", sequelize.col("Reviews.stars")),
-                    "avgRating"
-                ],
-                [sequelize.literal("Images.url"), "previewImage"]
-            ]
-        },
-        include: [
-            {
-                model: Review,
-                attributes: []
-            },
-            {
-                model: Image,
-                where: {
-                    previewImage: true
-                },
-                attributes: []
-            }
-        ],
-        group: ['Spot.id']
-    })
-
-    res.json({ Spots })
-})
-
 router.get('/current', async (req, res) => {
     const { token } = req.cookies;
     let decodedUser = jwt.decode(token)
@@ -77,5 +45,84 @@ router.get('/current', async (req, res) => {
     res.json({ Spots: userSpot })
 
 })
+
+router.get('/:spotId', async (req, res) => {
+
+    const spot = await Spot.findOne({
+        attributes: {
+            include: [
+                [sequelize.fn("COUNT", sequelize.col("Reviews.id")),
+                    "numReviews"],
+                [
+                    sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+                    "avgStarRating"
+                ],
+
+            ]
+        },
+        include: [
+            {
+                model: Review,
+                attributes: []
+            },
+
+
+        ],
+        group: ['Spot.id'],
+        where: { id: req.params.spotId },
+        raw: true,
+    })
+
+    let Images = await Image.findAll({
+        where: { spotId: req.params.spotId },
+        attributes: ["id", ["spotId", "imageableId"], "url"],
+        raw: true
+    })
+    let Owner = await User.findOne({
+        include: [{ model: Spot, where: { id: req.params.spotId }, attributes: [] }],
+        raw: true,
+        attributes: ["id", "firstName", "lastName"]
+    })
+
+    spot.Images = Images
+    spot.Owner = Owner
+
+    res.json(spot)
+
+})
+
+
+router.get('/', async (req, res) => {
+    const Spots = await Spot.findAll({
+
+        attributes: {
+            include: [
+                [
+                    sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+                    "avgRating"
+                ],
+                [sequelize.literal("Images.url"), "previewImage"]
+            ]
+        },
+        include: [
+            {
+                model: Review,
+                attributes: []
+            },
+            {
+                model: Image,
+                where: {
+                    previewImage: true
+                },
+                attributes: []
+            }
+        ],
+        group: ['Spot.id']
+    })
+
+    res.json({ Spots })
+})
+
+
 
 module.exports = router;
