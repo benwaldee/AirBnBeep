38c36f2da036c07ec3221a22c75f971a13d9556a
ben waldee <b1.waldee@gmail.com>
2022-08-02 14:30:38 -0500

    get all spots done

--- backend/db/seeders/20220802165247-images-data.js
+++ backend/db/seeders/20220802165247-images-data.js
@@ -14,84 +14,84 @@ module.exports = {
 
     await queryInterface.bulkInsert('Images', [
       {
-        url: 'www.example.com',
+        url: 'www.1example.com',
         previewImage: true,
         spotId: 1,
         reviewId: null,
         userId: 1
       },
       {
-        url: 'www.example.com',
+        url: 'www.2fexample.com',
         previewImage: false,
         spotId: 2,
         reviewId: null,
         userId: 2
       },
       {
-        url: 'www.example.com',
+        url: 'www.3example.com',
         previewImage: true,
         spotId: 3,
         reviewId: null,
         userId: 3
       },
       {
-        url: 'www.example.com',
+        url: 'www.4example.com',
         previewImage: true,
         spotId: 4,
         reviewId: null,
         userId: 4
       },
       {
-        url: 'www.example.com',
+        url: 'www.5example.com',
         previewImage: true,
         spotId: 5,
         reviewId: null,
         userId: 5
       },
       {
-        url: 'www.example.com',
+        url: 'www.6example.com',
         previewImage: true,
         spotId: 6,
         reviewId: null,
         userId: 6
       },
       {
-        url: 'www.example.com',
+        url: 'www.7texample.com',
         previewImage: true,
         spotId: 7,
         reviewId: null,
         userId: 7
       },
       {
-        url: 'www.example.com',
+        url: 'www.7fexample.com',
         previewImage: false,
         spotId: 7,
         reviewId: null,
         userId: 7
       },
       {
-        url: 'www.example.com',
+        url: 'www.8example.com',
         previewImage: true,
         spotId: 8,
         reviewId: null,
         userId: 8
       },
       {
-        url: 'www.example.com',
+        url: 'www.9example.com',
         previewImage: true,
         spotId: 9,
         reviewId: null,
         userId: 9
       },
       {
-        url: 'www.review.com',
+        url: 'www.2review.com',
         previewImage: true,
         spotId: null,
         reviewId: 2,
         userId: 1
       },
       {
-        url: 'www.review.com',
+        url: 'www.5review.com',
         previewImage: true,
         spotId: null,
         reviewId: 5,
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,13 +2,40 @@
 const express = require('express')
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { User, Spot, Booking, Review, Image } = require('../../db/models');
+const { User, Spot, Booking, Review, Image, sequelize } = require('../../db/models');
 const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.json('hello world')
+    const Spots = await Spot.findAll({
+
+        attributes: {
+            include: [
+                [
+                    sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+                    "avgRating"
+                ],
+                [sequelize.literal("Images.url"), "previewImage"]
+            ]
+        },
+        include: [
+            {
+                model: Review,
+                attributes: []
+            },
+            {
+                model: Image,
+                where: {
+                    previewImage: true
+                },
+                attributes: []
+            }
+        ],
+        group: ['Spot.id']
+    })
+
+    res.json({ Spots })
 })
 
 module.exports = router;
