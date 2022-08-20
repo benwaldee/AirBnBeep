2e4304a5347a677eaa84a9761ab129a93c121eda
ben waldee <b1.waldee@gmail.com>
2022-08-19 19:28:30 -0500

    pub

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,12 +13,12 @@ if (process.env.NODE_ENV === 'production') {
     router.get('/', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
         return res.sendFile(
-            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+            path.resolve(__dirname, '../../frontend', 'public', 'index.html')
         );
     });
 
     // Serve the static assets in the frontend's public folder
-    router.use(express.static(path.resolve("../frontend/build")));
+    router.use(express.static(path.resolve("../frontend/public")));
 
     // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
