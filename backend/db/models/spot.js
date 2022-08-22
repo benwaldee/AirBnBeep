ee430db24bb4c57ab2c95b91e64c01390fa2abf5
ben waldee <b1.waldee@gmail.com>
2022-08-22 16:56:01 -0500

    description char icnrease

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -66,7 +66,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(2000),
       allowNull: false
     },
     price: {
